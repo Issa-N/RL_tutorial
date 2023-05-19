a937784402b50392122d898acb613be09a1a2f44
Issa-N <82076084+Issa-N@users.noreply.github.com>
2023-05-19 19:29:22 +0900

    Update print.py

--- print.py
+++ print.py
@@ -1,5 +1,5 @@
 #print function
-def PrintAnswer(num, ans);
+def PrintAnswer(num, ans):
     print(num[0],"+",num[1],"=",ans[0])
     print(num[0],"-",num[1],"=",ans[1])
     print(num[0],"*",num[1],"=",ans[2])
