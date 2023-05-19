5fafe47e168e2dc4c3109a4527b122a21e6d9744
Shunsuke Araki <shunsuke-a@ws19is108.naist.jp>
2023-05-19 15:52:30 +0900

    Upload input.py

---
+++ input.py
@@ -0,0 +1,10 @@
+def Input():
+    while -1:
+        inputString = input('Please input two number(Num1 Num2):') 
+        inputStringSplite = inputString.split(' ')
+        inputNumber = []
+        for num in inputStringSplite:
+            if num.isdecimal():
+                inputNumber.append(int(num))
+        if len(inputNumber) >= 2 and inputNumber[1] != 0:
+            return inputNumber
