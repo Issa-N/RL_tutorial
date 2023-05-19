d83689334a37ff631f080df74f4c82ce1f84e71b
Tinoeike <kajiwara.ryutaro.ku9@is.naist.jp>
2023-05-19 17:13:30 +0900

    make substraction

--- calc.py
+++ calc.py
@@ -4,4 +4,7 @@ def Clac(inputNumber):
     #calc addition
     result_add = inputNumber[0] + inputNumber[1]
 
+    #calc substraction
+    result_sub = inputNumber[0] - inputNumber[1]
+
     return result
